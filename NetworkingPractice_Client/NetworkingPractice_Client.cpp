22170302a5dffcb2d6b36f822b828385ed223ba9
j-cob44 <jacoburton44@gmail.com>
2023-04-03 19:05:51 -0600

    Separate Sending and Receiving on Client
    Multithreaded Sending to allow the user to send data while receiving data. Since the server is constantly sending data, the client knows its still connected by receiving data.

--- NetworkingPractice_Client/NetworkingPractice_Client.cpp
+++ NetworkingPractice_Client/NetworkingPractice_Client.cpp
@@ -6,9 +6,41 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <thread>
 #include <winsock2.h>
 
 #pragma comment(lib, "ws2_32.lib") // link with Winsock library
+
+// Handle Sending
+void handleSending(SOCKET clientSocket) {
+    bool connected = true;
+    while (connected) {
+        // Send Message to the Server
+        std::string message;
+        std::getline(std::cin, message);
+        if (message == "exit") {
+            connected = false;
+        }
+        else if (!message.empty()) {
+            int send_status = send(clientSocket, message.c_str(), 1024, 0);
+            if (send_status == SOCKET_ERROR) {
+                std::cerr << "Send failed: " << WSAGetLastError() << std::endl;
+                connected = false;
+            }
+        }
+        else {
+            int send_status = send(clientSocket, "", 0, 0);
+            if (send_status == SOCKET_ERROR) {
+                std::cerr << "Send failed: " << WSAGetLastError() << std::endl;
+                connected = false;
+            }
+        }
+    }
+
+    // Either by error or by choice, close connection
+    closesocket(clientSocket);
+}
 
 int main() {
     // Initialize Winsock
@@ -46,43 +78,34 @@ int main() {
 
     std::cout << "Connected to server" << std::endl;
 
-    // TODO: Send and receive messages to/from the server
-    bool connected = true;
-    while (connected) {
+    // Vector of threads for sending
+    std::thread sendingThread(handleSending, clientSocket);
 
-        // Receive Message from the server
-        char buffer[1024] = { 0 };
+    bool connected = true;
+    char buffer[1024];
+    while (connected) {
+        // Client is constantly receiving data from the server, if something happens we will find out here
+        ZeroMemory(buffer, 1024);
         int recv_status = recv(clientSocket, buffer, sizeof(buffer), 0);
         if (recv_status == SOCKET_ERROR) {
-			std::cerr << "Receive failed: " << WSAGetLastError() << std::endl;
-			closesocket(clientSocket);
-			WSACleanup();
-			return 1;
-		}
+            //std::cerr << "Receive failed: " << WSAGetLastError() << std::endl;
+            connected = false;
+        }
         else if (recv_status == 0) {
-			std::cout << "Server disconnected" << std::endl;
-			connected = false;
-		}
+            //std::cout << "Server disconnected" << std::endl;
+            connected = false;
+        }
         else {
-			std::cout << "Server says: " << buffer << std::endl;
-		}
+            std::cout << "Server says: " << buffer << std::endl;
+        }
 
-        // Send Message to the Server
-        std::string message;
-        std::getline(std::cin, message);
-        if (message == "exit") {
-			connected = false;
-		}
-        else if (!message.empty()) {
-			int send_status = send(clientSocket, message.c_str(), message.size() + 1, 0);
-            if (send_status == SOCKET_ERROR) {
-				std::cerr << "Send failed: " << WSAGetLastError() << std::endl;
-				closesocket(clientSocket);
-				WSACleanup();
-				return 1;
-			}
-		}
+        ZeroMemory(buffer, 1024);
     }
+
+    // End Sending Thread
+    sendingThread.join();
+
+    std::cout << "Disconnected from Server." << std::endl;
 
     // Clean up
     closesocket(clientSocket);
